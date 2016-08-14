c95c8e80110645005c6ad4ae0fbdc243dca689b6
None Business <none@business.com>
2016-08-13 19:39:17 -0500

    Commit after JF2 Threading - Executors.

--- src/greetings/HelloWorld.java
+++ src/greetings/HelloWorld.java
@@ -9,14 +9,25 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HelloWorld {
 
 
     public static void main(String[] args) {
         ContinuousPrinter cp = new ContinuousPrinter();
-        Thread thread = new Thread(cp);
-        thread.start();
+//        Thread thread = new Thread(cp);
+//        thread.start();
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        executor.submit(cp);
+        executor.submit(cp);
+        executor.submit(cp);
+        executor.submit(cp);
+        executor.submit(cp);
+        executor.submit(cp);
+        executor.shutdown();
 
         for (int i = 0; i < 100; i++) {
             System.out.println("Main Thread: " + i);
